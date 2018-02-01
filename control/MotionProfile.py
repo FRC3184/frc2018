954d5a4c1bc1901a0088f0dfb0fe1a9ecafd55f4
Nick Schatz <nick@nickschatz.com>
2018-01-31 22:41:58 -0600

    Start building custom motion profiling system

---
+++ control/MotionProfile.py
@@ -0,0 +1,98 @@
+from typing import List, Sequence
+
+import time
+from ctre import ControlMode
+from ctre.talonsrx import TalonSRX
+
+from control import robot_time
+
+
+class TrajectoryPoint:
+    def __init__(self, position, velocity):
+        self.position = position
+        self.velocity = velocity
+        self.time = 0  # TODO
+
+
+class MotionProfile:
+    """
+    Represents a trapezoidal motion profile
+    """
+    def __init__(self, start, end, acc, cruise_speed, feedforward, frequency=100):
+        # https://www.desmos.com/calculator/ponjr7cwze
+
+        dist = end - start  # TODO Check signs
+
+        ramp_time = cruise_speed / acc
+        ramp_dist = acc * ramp_time ** 2 / 2
+        cruise_dist = dist - 2 * ramp_dist
+        cruise_time = cruise_dist / cruise_speed
+
+        if cruise_dist < 0:
+            # All ramp, no cruise. Fix parameters to match
+            cruise_time = 0
+            cruise_dist = 0
+            ramp_time = (dist / acc) ** 0.5
+            ramp_dist = dist / 2
+
+        time = cruise_time + 2 * ramp_time
+
+        def get_pos(t):
+            if t <= ramp_time:
+                return acc * t ** 2 / 2
+            elif t <= ramp_time + cruise_time:
+                return ramp_dist + (t - ramp_time) * get_vel(ramp_time)
+            else:
+                tp = (t - ramp_time - cruise_time)
+                return ramp_dist + cruise_dist + get_vel(ramp_time + cruise_time) * tp - acc * tp ** 2 / 2
+
+        def get_vel(t):
+            if t <= ramp_time:
+                return get_acc(t) * t
+            elif t <= ramp_time + cruise_time:
+                return cruise_speed
+            else:
+                tp = (t - ramp_time - cruise_time)
+                return get_vel(ramp_time + cruise_time) - acc * tp
+
+        def get_acc(t):
+            if t <= ramp_time:
+                return acc
+            elif t <= ramp_time + cruise_time:
+                return 0
+            else:
+                return -acc
+
+        self.points = []
+        for i in range(int(time * frequency) + 1):
+            t = i / frequency
+            self.points.append(TrajectoryPoint(position=get_pos(t),
+                                               velocity=get_vel(t)))
+
+
+class MotionProfileThread:
+    def __init__(self, target: TalonSRX, points: List[TrajectoryPoint]):
+        self.timeoutMs = 0
+        self.target = target
+        self.points = points
+        self.margin = 0
+        self.slotIdx = 0
+
+    def start(self):
+        self.time_begin = time.time().now()
+
+    def run(self):
+        self.current_point = TrajectoryPoint(0,0)
+        time_point = self.current_point.time
+
+        if self.target.getClosedLoopError(0) < self.margin:
+            self.time_begin = time.time().now()
+
+        if time.time().now() - self.time_begin > time_point:
+            self.current_point = self.points.pop()
+            vel = self.current_point.velocity
+            constant = 0
+            self.target.config_kF(self.slotIdx, vel + constant, self.timeoutMs)
+            self.target.set(ControlMode.Position, self.current_point.position)
+
+        robot_time.sleep(millis=5)

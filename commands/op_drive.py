0a4689581c45fb2888e3c959d84ec6d5344c2704
Nathan Laptop <njm5865@gmail.com>
2018-02-11 13:32:40 -0600

    Added linear scaling to drive power. changed the elevator spool radius to 1.

--- commands/op_drive.py
+++ commands/op_drive.py
@@ -1,16 +1,18 @@
 from wpilib.command import Command
 
 import mathutils
-import systems
 from control import OI
 from systems.drivetrain import Drivetrain
+from systems.elevator import Elevator
+from systems.elevator import TOP_EXTENT
 
 
 class OpDriveCommand(Command):
-    def __init__ (self, drivetrain: Drivetrain):
+    def __init__ (self, drivetrain: Drivetrain, elevator: Elevator):
         super().__init__ ()
         self.requires(drivetrain)
         self.drivetrain = drivetrain
+        self.elevator = elevator
 
     def initialize(self):
         pass
@@ -18,8 +20,9 @@ class OpDriveCommand(Command):
     def execute(self):
         oi = OI.get()
         drive = self.drivetrain
+        elevator = self.elevator
 
-        power = mathutils.signed_power(oi.get_left_power(), 2)
+        power = self.get_speed_scale(elevator.get_elevator_position())*mathutils.signed_power(oi.get_left_power(), 2)
         turn = mathutils.signed_power(oi.get_turn_command(), 2)
 
         drive.arcade_drive(power, turn * 0.5)
@@ -29,3 +32,8 @@ class OpDriveCommand(Command):
 
     def isFinished(self):
         return False
+
+    def get_speed_scale(self, current_height):
+        speed_scale = (-0.8/(TOP_EXTENT))*current_height+1
+
+        return speed_scale

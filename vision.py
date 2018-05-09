b1ab1fe82e9940cd655e2e2c6f25a149d277dea6
Nick Schatz <nick@nickschatz.com>
2018-05-09 16:34:47 -0500

    Fix passthrough vision.py

--- vision.py
+++ vision.py
@@ -1,5 +1,10 @@
+import numpy as np
+
 from cscore import CameraServer
 
+
+width = 160
+height = 120
 
 def main():
     cs = CameraServer.getInstance()
@@ -7,4 +12,26 @@ def main():
 
     # Capture from the first USB Camera on the system
     camera = cs.startAutomaticCapture()
-    camera.setResolution(160, 120)
+    camera.setResolution(width, height)
+
+    cvSink = cs.getVideo()
+
+    # (optional) Setup a CvSource. This will send images back to the Dashboard
+    outputStream = cs.putVideo("Rectangle", width, height)
+
+    # Allocating new images is very expensive, always try to preallocate
+    img = np.zeros(shape=(height, width, 3), dtype=np.uint8)
+
+    while True:
+        # Tell the CvSink to grab a frame from the camera and put it
+        # in the source image.  If there is an error notify the output.
+        time, img = cvSink.grabFrame(img)
+        if time == 0:
+            # Send the output the error.
+            outputStream.notifyError(cvSink.getError());
+            # skip the rest of the current iteration
+            continue
+
+        # Give the output stream a new image to display
+
+        outputStream.putFrame(img)
